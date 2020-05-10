f099be9d4419bb6982cbae489ba989ba593022df
Susmoy <susmoymukherjee@gmail.com>
2020-05-11 02:12:03 +0530

    Updates

---
+++ precache-manifest.8005d2a7c18d567755b88bee12a241f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0cbd367e133592e958d90bbd737884",
+    "url": "/reactweatherapp/index.html"
+  },
+  {
+    "revision": "216cf739b61bd389a4ac",
+    "url": "/reactweatherapp/static/css/main.f14cfd22.chunk.css"
+  },
+  {
+    "revision": "f84955655200fe3c5b4f",
+    "url": "/reactweatherapp/static/js/2.0c42346a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactweatherapp/static/js/2.0c42346a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216cf739b61bd389a4ac",
+    "url": "/reactweatherapp/static/js/main.352e3324.chunk.js"
+  },
+  {
+    "revision": "cdd3acf3abf42ddcc598",
+    "url": "/reactweatherapp/static/js/runtime-main.45e3665e.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/reactweatherapp/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/reactweatherapp/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
